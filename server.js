390bebdcbb9b9b34523dc07a5787f4533fb52c9a
sdarwin <samuel.d.darwin@gmail.com>
2019-03-07 12:36:42 +0000

    staging branch

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Version 2. Hello world from a Node.js app!')
+    res.send('Staging. Hello world from a Node.js app!')
 })
 app.listen(3000, () => {
     console.log('Server is up on 3000')
